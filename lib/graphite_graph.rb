6692a2541ad168ed180b9256ab8b45d8a1fec748
R.I.Pienaar <rip@devco.net>
2011-10-15 10:37:34 +0100

    Bring up to date with the DSL project - add Holt Winters forecasts

--- lib/graphite_graph.rb
+++ lib/graphite_graph.rb
@@ -24,6 +24,17 @@
 #
 # result is an array of graph object with a lot of field hashes and a to_url
 # method that makes the img src url
+#
+# Since graphite 0.9.9 you can create Holt-Winters Forecast graphs using a simple
+# helper:
+#
+# hw_predict  :foo, :data => "sumSeries(tc*.site.users.male)",
+#                   :alias => "Male",
+#                   :derivative => false,
+#                   :color => "yellow"
+#
+# This creates 3 data fields - 1 the actual data, 1 forecast and the confidence
+# bands, an example can be seen in samples
 class GraphiteGraph
     attr_reader :info, :properties, :targets, :target_order
 
@@ -82,6 +93,32 @@ class GraphiteGraph
         @service_mode = false
     end
 
+    # add 2 fields using the Holt-Winters Confidence Band prediction model
+    #
+    #    hw_predict  :foo, :data => "some.data.item",
+    #                      :derivative => false,
+    #                      :color => "yellow"
+    #
+    def hw_predict(name, args)
+        raise ":data is needed as an argument to a Holt-Winters Confidence forecast" unless args[:data]
+
+        forecast_args = args.clone
+        bands_args = args.clone
+
+        forecast_args[:data] = "holtWintersForecast(#{forecast_args[:data]})"
+        forecast_args[:alias] = "#{args[:alias]} Forecast"
+        forecast_args[:color] = "blue"
+        field "#{name}_forecast", forecast_args
+
+        bands_args[:data] = "holtWintersConfidenceBands(#{bands_args[:data]})"
+        bands_args[:color] = "grey"
+        bands_args[:dashed] = true
+        bands_args[:alias] = "#{args[:alias]} Confidence"
+        field "#{name}_bands", bands_args
+
+        field name, args
+    end
+
     def field(name, args)
         raise "A field called #{name} already exist for this graph" if targets.include?(name)
 
@@ -121,14 +158,14 @@ class GraphiteGraph
                 graphite_target = "scale(#{graphite_target},#{target[:scale]})" if target[:scale]
                 graphite_target = "drawAsInfinite(#{graphite_target})" if target[:line]
 
+                graphite_target = "color(#{graphite_target},\"#{target[:color]}\")" if target[:color]
+                graphite_target = "dashed(#{graphite_target})" if target[:dashed]
+
                 if target[:alias]
                     graphite_target = "alias(#{graphite_target},\"#{target[:alias]}\")"
                 else
                     graphite_target = "alias(#{graphite_target},\"#{name.to_s.capitalize}\")"
                 end
-
-                graphite_target = "color(#{graphite_target},\"#{target[:color]}\")" if target[:color]
-                graphite_target = "dashed(#{graphite_target})" if target[:dashed]
 
                 url_parts << "target=#{graphite_target}"
             end
