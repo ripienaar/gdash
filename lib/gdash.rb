f6b2400a04b34160cff5ea937084dd854c1ec73b
R.I.Pienaar <rip@devco.net>
2011-10-19 12:38:16 +0100

    - Small general clean ups - Whitespace change

--- lib/gdash.rb
+++ lib/gdash.rb
@@ -4,48 +4,58 @@ require 'yaml'
 require 'erb'
 
 class GDash
-    require 'gdash/dashboard'
-    require 'gdash/monkey_patches'
-    require 'gdash/sinatra_app'
-    require 'graphite_graph'
+  require 'gdash/dashboard'
+  require 'gdash/monkey_patches'
+  require 'gdash/sinatra_app'
+  require 'graphite_graph'
 
-    attr_reader :graphite_base, :graphite_render, :dash_templates, :height, :width
+  attr_reader :graphite_base, :graphite_render, :dash_templates, :height, :width
 
-    def initialize(graphite_base, render_url, dash_templates, width=500, height=250)
-        @graphite_base = graphite_base
-        @graphite_render = [@graphite_base, "/render/"].join
-        @dash_templates = dash_templates
-        @height = height
-        @width = width
+  def self.categories(dash_templates)
+    Dir.entries(dash_templates).select do |dir|
+      dir if File.directory?(File.join(dash_templates, dir)) && !dir.match(/^\./)
+    end.sort
+  end
 
-        raise "Dashboard templates directory #{@dash_templates} does not exist" unless File.directory?(@dash_templates)
-    end
+  def initialize(graphite_base, render_url, dash_templates, width=500, height=250)
+    @graphite_base = graphite_base
+    @graphite_render = [@graphite_base, "/render/"].join
+    @dash_templates = dash_templates
+    @height = height
+    @width = width
 
-    def dashboard(name, width=nil, height=nil)
-        width ||= @width
-        height ||= @height
+    raise "Dashboard templates directory #{@dash_templates} does not exist" unless File.directory?(@dash_templates)
+  end
 
-        Dashboard.new(name, dash_templates, width, height)
-    end
+  def dashboard(name, width=nil, height=nil)
+    width ||= @width
+    height ||= @height
 
-    def list
-        dashboards.map {|dash| dash[:link]}
-    end
+    Dashboard.new(name, dash_templates, width, height)
+  end
 
-    def dashboards
-        dashboards = []
+  def list
+    dashboards.map {|dash| dash[:link]}
+  end
 
-        Dir.entries(dash_templates).each do |dash|
-            begin
-                yaml_file = File.join(dash_templates, dash, "dash.yaml")
-                if File.exist?(yaml_file)
-                    dashboards << YAML.load_file(yaml_file).merge({:link => dash})
-                end
-            rescue Exception => e
-                p e
-            end
+  def count
+    list.size
+  end
+
+  def dashboards
+    dashboards = []
+
+    Dir.entries(dash_templates).each do |dash|
+      begin
+        yaml_file = File.join(dash_templates, dash, "dash.yaml")
+        if File.exist?(yaml_file)
+          dashboards << YAML.load_file(yaml_file).merge({:link => dash})
         end
-
-        dashboards.sort_by{|d| d[:name]}
+      rescue Exception => e
+        p e
+      end
     end
+
+    dashboards.sort_by{|d| d[:name]}
+  end
 end
--- lib/gdash/sinatra_app.rb
+++ lib/gdash/sinatra_app.rb
@@ -1,94 +1,95 @@
 class GDash
-    class SinatraApp < ::Sinatra::Base
-        def initialize(graphite_base, graph_templates, options = {})
-            # where the whisper data is
-            @whisper_dir = options.delete(:whisper_dir) || "/var/lib/carbon/whisper"
+  class SinatraApp < ::Sinatra::Base
+    def initialize(graphite_base, graph_templates, options = {})
+      # where the whisper data is
+      @whisper_dir = options.delete(:whisper_dir) || "/var/lib/carbon/whisper"
 
-            # where graphite lives
-            @graphite_base = graphite_base
+      # where graphite lives
+      @graphite_base = graphite_base
 
-            # where the graphite renderer is
-            @graphite_render = [@graphite_base, "/render/"].join
+      # where the graphite renderer is
+      @graphite_render = [@graphite_base, "/render/"].join
 
-            # where to find graph, dash etc templates
-            @graph_templates = graph_templates
+      # where to find graph, dash etc templates
+      @graph_templates = graph_templates
 
-            # the dash site might have a prefix for its css etc
-            @prefix = options.delete(:prefix) || ""
+      # the dash site might have a prefix for its css etc
+      @prefix = options.delete(:prefix) || ""
 
-            # how many columns of graphs do you want on a page
-            @graph_columns = options.delete(:graph_columns) || 2
+      # how many columns of graphs do you want on a page
+      @graph_columns = options.delete(:graph_columns) || 2
 
-            # how wide each graph should be
-            @graph_width = options.delete(:graph_width) || 500
+      # how wide each graph should be
+      @graph_width = options.delete(:graph_width) || 500
 
-            # how hight each graph sould be
-            @graph_height = options.delete(:graph_height) || 250
+      # how hight each graph sould be
+      @graph_height = options.delete(:graph_height) || 250
 
-            # Dashboard title
-            @dash_title = options.delete(:title) || "Graphite Dashboard"
+      # Dashboard title
+      @dash_title = options.delete(:title) || "Graphite Dashboard"
 
-            @top_level = Hash.new
-            Dir.entries(@graph_templates).each do |category|
-              if File.directory?("#{@graph_templates}/#{category}")
-                unless ("#{category}" =~ /^\./ )
-                  @top_level["#{category}"] = GDash.new(@graphite_base, "/render/", File.join(@graph_templates, "/#{category}"), @graph_width, @graph_height)
-                end
-              end
-            end
+      @top_level = Hash.new
 
-            super()
-        end
+      GDash.categories(@graph_templates).each do |category|
+        @top_level[category] = GDash.new(@graphite_base, "/render/", File.join(@graph_templates, category), @graph_width, @graph_height)
+      end
 
-        set :static, true
-        set :views, File.join(File.expand_path(File.dirname(__FILE__)), "../..", "views")
-        set :public_folder, File.join(File.expand_path(File.dirname(__FILE__)), "../..", "public")
-
-        get '/' do
-            if @top_level.empty?
-                @error = "No dashboards found in the templates directory"
-            end
-
-            erb :index
-        end
-
-	get '/:category/:dash/full/?*' do
-            params["splat"] = params["splat"].first.split("/")
-
-            params["columns"] = params["splat"][0].to_i || @graph_columns
-
-            if params["splat"].size == 3
-                width = params["splat"][1].to_i
-                height = params["splat"][2].to_i
-            else
-                width = @graph_width
-                height = @graph_height
-            end
-
-
-            if @top_level["#{params[:category]}"].list.include?(params[:dash])
-                @dashboard = @top_level[@params[:category]].dashboard(params[:dash], width, height)
-            else
-                @error = "No dashboard called #{params[:dash]} found in #{params[:category]}/#{@top_level[params[:category]].list.join ','}"
-            end
-
-            erb :full_size_dashboard, :layout => false
-	end
-
-        get '/:category/:dash/' do
-            if @top_level["#{params[:category]}"].list.include?(params[:dash])
-                @dashboard = @top_level[@params[:category]].dashboard(params[:dash])
-            else
-                @error = "No dashboard called #{params[:dash]} found in #{params[:category]}/#{@top_level[params[:category]].list.join ','}."
-            end
-
-            erb :dashboard
-        end
-
-        helpers do
-            include Rack::Utils
-
-            alias_method :h, :escape_html
-        end
+      super()
     end
+
+    set :static, true
+    set :views, File.join(File.expand_path(File.dirname(__FILE__)), "../..", "views")
+    set :public_folder, File.join(File.expand_path(File.dirname(__FILE__)), "../..", "public")
+
+    get '/' do
+      if @top_level.empty?
+        @error = "No dashboards found in the templates directory"
+      else
+        count = @top_level.keys.inject(0) {|i,v| i + @top_level[v].count}
+
+        @error = "No dashboards found in the templates directory" if count == 0
+      end
+
+      erb :index
+    end
+
+    get '/:category/:dash/full/?*' do
+      params["splat"] = params["splat"].first.split("/")
+
+      params["columns"] = params["splat"][0].to_i || @graph_columns
+
+      if params["splat"].size == 3
+        width = params["splat"][1].to_i
+        height = params["splat"][2].to_i
+      else
+        width = @graph_width
+        height = @graph_height
+      end
+
+
+      if @top_level["#{params[:category]}"].list.include?(params[:dash])
+        @dashboard = @top_level[@params[:category]].dashboard(params[:dash], width, height)
+      else
+        @error = "No dashboard called #{params[:dash]} found in #{params[:category]}/#{@top_level[params[:category]].list.join ','}"
+      end
+
+      erb :full_size_dashboard, :layout => false
+    end
+
+    get '/:category/:dash/' do
+      if @top_level["#{params[:category]}"].list.include?(params[:dash])
+        @dashboard = @top_level[@params[:category]].dashboard(params[:dash])
+      else
+        @error = "No dashboard called #{params[:dash]} found in #{params[:category]}/#{@top_level[params[:category]].list.join ','}."
+      end
+
+      erb :dashboard
+    end
+
+    helpers do
+      include Rack::Utils
+
+      alias_method :h, :escape_html
+    end
+  end
 end
