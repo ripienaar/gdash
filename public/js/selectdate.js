f787b58a237cdfd3b43891dac724e119ef5f6e01
Holger Amann <holger@sauspiel.de>
2013-02-06 09:45:50 +0100

    Adding a datetimepicker

---
+++ public/js/selectdate.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  $('#dateTimePicker').hide();
+  $('#toggleDateTimePicker').click(function() {
+    $('#dateTimePicker').toggle(400);
+  });
+});
+
+$(function() {
+  var startDateTextBox = $('#dt_from');
+  var endDateTextBox = $('#dt_to');
+  startDateTextBox.datetimepicker({ 
+    onClose: function(dateText, inst) {
+      if (endDateTextBox.val() != '') {
+        var testStartDate = startDateTextBox.datetimepicker('getDate');
+        var testEndDate = endDateTextBox.datetimepicker('getDate');
+        if (testStartDate > testEndDate)
+          endDateTextBox.datetimepicker('setDate', testStartDate);
+      }
+      else {
+        endDateTextBox.val(dateText);
+      }
+    },
+    onSelect: function (selectedDateTime){
+      endDateTextBox.datetimepicker('option', 'minDate', startDateTextBox.datetimepicker('getDate') );
+    }
+  });
+  endDateTextBox.datetimepicker({ 
+    onClose: function(dateText, inst) {
+      if (startDateTextBox.val() != '') {
+        var testStartDate = startDateTextBox.datetimepicker('getDate');
+        var testEndDate = endDateTextBox.datetimepicker('getDate');
+        if (testStartDate > testEndDate)
+          startDateTextBox.datetimepicker('setDate', testEndDate);
+      }
+      else {
+        startDateTextBox.val(dateText);
+      }
+    },
+    onSelect: function (selectedDateTime){
+      startDateTextBox.datetimepicker('option', 'maxDate', endDateTextBox.datetimepicker('getDate') );
+    }
+  });
+});
+
+function selectDt() {
+  var dt_from = $('#dt_from').datetimepicker('getDate');
+  var dt_to = $('#dt_to').datetimepicker('getDate');
+  window.location = buildGraphiteDateUrl(dt_from, dt_to);
+  return true;
+}
+
+function buildGraphiteDateUrl(dt_from, dt_to)
+{
+  from = buildGraphiteDateString(dt_from);
+  to = buildGraphiteDateString(dt_to);
+  params =  "?&from=" + from + "&until=" + to;
+  newurl = document.URL.replace(/#/g, '');
+  regex = /(\?&(from|until)=.+)/g;
+  if (newurl.match(regex)) {
+    newurl = newurl.replace(regex,"");
+  }
+  return newurl + params;
+}
+
+function buildGraphiteDateString(date)
+{
+  val = zeroPad(date.getHours()) + "%3A" + zeroPad(date.getMinutes()) + "_" + date.getFullYear() + zeroPad(date.getMonth()+1) + zeroPad(date.getDate());
+  return val;
+}
+
+function zeroPad(num) {
+	num = "" + num
+	if (num.length == 1) {
+		num = "0" + num
+	}
+	return num
+}
